42ed7ffdc064a4a79acd171fb3dc1f0da5c32b8e
rizaladt <rzladt67@gmail.com>
2025-07-17 15:22:19 +0700

    change some settings

--- smart_land/settings.py
+++ smart_land/settings.py
@@ -37,7 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'authentication',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_'
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
