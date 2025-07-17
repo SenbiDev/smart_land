7a34fa6841535cb9926b68521894abdb9347586d
rizaladt <rzladt67@gmail.com>
2025-07-17 15:19:53 +0700

    add authentication models

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.conf import settings
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    ROLE_CHOICES=[
+        ('Superadmin', 'Superadmin'),
+        ('Admin', 'Admin'),
+        ('Oprator', 'Oprator'),
+        ('Investor', 'Investor'),
+        ('Viewer', 'Viewer'),
+    ]
+    role = models.CharField(max_length=100, choices=ROLE_CHOICES, default='user')
+
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name='groups',
+        blank=True,
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+        related_name="customuser_set",
+        related_query_name="user",
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name='user permissions',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_name="customuser_set",
+        related_query_name="user",
+    )
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    website = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.user.username
+    
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
