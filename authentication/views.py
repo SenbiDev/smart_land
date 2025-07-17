d68fbf7f80d11e4717ec2e692ce2c6e63d472895
rizaladt <rzladt67@gmail.com>
2025-07-17 15:20:39 +0700

    add authentication views

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
-
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
+from .models import CustomUser, Profile
+from .serializers import (
+    UserSerializer,
+    RegisterSerializer,
+    ProfileSerializer,
+)
+from django.contrib.auth import authenticate
 # Create your views here.
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def login_view(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    user = authenticate(username=username, password=password)
+    if user:
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+            'user': {
+                'id': user.id,
+                'username': user.username,
+                'email': user.email,
+                'role': user.role
+            }
+        })
+    return Response({'error': 'Invalid credentials'}, status=status.HTTP_406_NOT_ACCEPTABLE)
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def register_view(request):
+    serializer = RegisterSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            'user': RegisterSerializer(user).data,
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
