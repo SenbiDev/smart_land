90ed9b9ee16d6ff036a826f92add66929b7111ad
raflir13 <rafli.ramdhani1313@gmail.com>
2025-07-23 12:05:32 +0700

    add and change urls

--- asset/urls.py
+++ asset/urls.py
@@ -2,11 +2,10 @@ from django.urls import path
 from . import views  
 
 urlpatterns = [
-    path('kategori/', views.list_kategori),
-    path('kategori/tambah', views.tambah_kategori),
     path('Aset/', views.list_aset),
     path('aset/tambah', views.tambah_aset),
     path('asset/<int:pk>/', views.asset_detail),
-    path('pemilik/', views.list_pemilik),
-    path('pemilik/tambah', views.tambah_pemilik),
+    path('owner/', views.list_owner),
+    path('owner/tambah', views.tambah_owner),
+    path('owner/<int:pk>/', views.owner_detail),
 ]
