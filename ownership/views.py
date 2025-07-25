9de29617163f4f0344aba171f3047a5a616092d6
rizaladt <rzladt67@gmail.com>
2025-07-25 15:03:27 +0700

    add precentage in ownership modul

--- ownership/views.py
+++ ownership/views.py
@@ -2,23 +2,37 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
+from django.db.models import Sum
 from .models import Ownership
 from .serializers import OwnershipSerializer
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 @api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticated])
 def ownership_list(request):
     if request.method == 'GET':
-        ownerships = Ownership.objects.all()
-        serializer = OwnershipSerializer(ownerships, many=True)
+        ownership = Ownership.objects.all()
+        serializer = OwnershipSerializer(ownership, many=True)
         return Response(serializer.data)
 
     elif request.method == 'POST':
         serializer = OwnershipSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            ownership = serializer.save()
+
+            asset_id = serializer.data['asset']
+            total_units = Ownership.objects.filter(asset=asset_id).aggregate(total=Sum('units'))['total'] or 0
+            
+            for o in Ownership.objects.filter(asset=asset_id):
+                o.ownership_precentage = round ((o.units / total_units) * 100, 2) if total_units > 0 else 0
+                o.save(update_fields=['ownership_precentage'])
+
+            ownership.refresh_from_db()
+        
+            update_serializer = OwnershipSerializer(ownership)
+            return Response(update_serializer.data, status=status.HTTP_201_CREATED)
+        
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
@@ -37,9 +51,22 @@ def ownership_detail(request, pk):
         serializer = OwnershipSerializer(ownership, data=request.data)
         if serializer.is_valid():
             serializer.save()
+
+            asset_id = serializer.data['asset']
+            total_units = Ownership.objects.filter(asset=asset_id).aggregate(total=Sum('units'))['total'] or 0
+            for o in Ownership.objects.filter(asset=asset_id):
+                o.ownership_precentage = round ((o.units / total_units) * 100, 2) if total_units > 0 else 0
+                o.save(update_fields=['ownership_precentage'])
+                
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         ownership.delete()
+
+        total_units = Ownership.objects.filter(asset=asset_id).aggregate(total=Sum('units'))['total'] or 0
+        for o in Ownership.objects.filter(asset=asset_id):
+            o.ownership_precentage = round ((o.units / total_units) * 100, 2) if total_units > 0 else 0
+            o.save(update_fields=['ownership_precentage'])
+                
         return Response(status=status.HTTP_204_NO_CONTENT)
