d21bfbf7dfd3637201e1cb5740a4b146de3940a4
raflir13 <rafli.ramdhani1313@gmail.com>
2025-07-28 15:05:30 +0700

    update views expense

--- expense/urls.py
+++ expense/urls.py
@@ -4,4 +4,5 @@ from .import views
 urlpatterns = [
     path('expenses/', views.list_expense),
     path('expenses/<int:pk>/', views.expense_detail),
+    path('funding/<int:funding_id>/expense-summary/', views.funding_expense_summary),
 ]
--- expense/views.py
+++ expense/views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status, permissions
 from .models import Expense
+from django.db.models import Sum
 from .serializers import ExpenseSerializer
 
 @api_view(['GET', 'POST'])
@@ -41,3 +42,34 @@ def expense_detail(request, pk):
     elif request.method == 'DELETE':
         expense.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+@api_view(['GET', 'POST'])
+@permission_classes([permissions.IsAuthenticated])
+def expense__list_create(request):
+    if request.method == 'GET':
+        project_id = request.GET.get('project_id')
+        funding_id = request.GET.get('funding_id')
+        asset_id = request.GET.get('asset_id')
+
+        expenses = Expense.objects.all()
+        if project_id : expenses = expenses.filter(project_id = project_id)
+        if funding_id : expenses = expenses.filter(funding_id = funding_id)
+        if asset_id : expenses = expenses.filter(asset_id = asset_id)
+        total = expenses.aaggregate(total=Sum('amount'))['total'] or 0 
+        
+        serializer = ExpenseSerializer(expenses, many=True)
+        return Response({
+            'total_amount': total,
+            'expenses': 
+        serializer.data
+        })
+    
+    elif request.method == 'POST':
+        serializer = ExpenseSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([permissions])
