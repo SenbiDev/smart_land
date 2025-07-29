45df9714c5c95b512f691266710d09ea22bbf9dc
raflir13 <rafli.ramdhani1313@gmail.com>
2025-07-29 15:02:06 +0700

    memperbaiki views expense

--- expense/urls.py
+++ expense/urls.py
@@ -3,6 +3,8 @@ from .import views
 
 urlpatterns = [
     path('expenses/', views.list_expense),
+    path('expensess/', views.expense_list_create),
     path('expenses/<int:pk>/', views.expense_detail),
+    path('expensesss/<int:pk>/', views.expense_detail_update_delete),
     path('expenses/<int:funding_id>/expense-summary/', views.funding_expense_summary),
 ]
--- expense/views.py
+++ expense/views.py
@@ -4,6 +4,7 @@ from rest_framework import status, permissions
 from .models import Expense
 from django.db.models import Sum
 from .serializers import ExpenseSerializer
+from funding.models import Funding
 
 @api_view(['GET', 'POST'])
 @permission_classes([permissions.IsAuthenticated])
@@ -47,7 +48,7 @@ def expense_detail(request, pk):
 @permission_classes([permissions.IsAuthenticated])
 def expense_list_create(request):
     if request.method == 'GET':
-        project_id = request.GET.get('project_id')
+        project_id = request.GET.get('project_id') 
         funding_id = request.GET.get('funding_id')
         asset_id = request.GET.get('asset_id')
 
@@ -55,23 +56,31 @@ def expense_list_create(request):
 
         if project_id: expenses = expenses.filter(project_id=project_id)
         if funding_id: expenses = expenses.filter(funding_id=funding_id)
-        if asset_id: expenses = expenses.filter(asset_id=asset_id)
-
-        total = expenses.aggregate(total=Sum('amount'))['total'] or 0
+        if asset_id: expenses = expenses.filter(asset_id=asset_id),
 
         serializer = ExpenseSerializer(expenses, many=True)
-        return Response({
-            'total_amount': total,
-            'expenses': serializer.data
-        })
-
+        return Response({serializer.data})
+    
     elif request.method == 'POST':
         serializer = ExpenseSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            funding_id = request.data.get('funding_id')
+            try:
+                funding = Funding.objects.get(id=funding_id)
+            except Funding.DoesNotExist:
+                return Response({'error': 'funding tidak di temukan'},status=status.HTTP_400_BAD_REQUEST)
+            expense_amount = serializer.validated_data['amount']
+            total_expense = Expense.objects.filter(funding_id=funding_id).aaggregate(total=Sum('amount'))['total'] or 0
 
+            if total_expense + expense_amount > funding.amount:
+                return Response({'error': 'Dana tidak cukup'},status=status.HTTP_400_BAD_REQUEST)
+            serializer.save()
+            funding.amount -= expense_amount
+            funding.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.data,status=status.HTTP_400_BAD_REQUEST)
+
+    
 @api_view(['GET', 'PUT', 'DELETE'])
 @permission_classes([permissions.IsAuthenticated])
 def expense_detail_update_delete(request, pk):
@@ -85,13 +94,29 @@ def expense_detail_update_delete(request, pk):
         return Response(serializer.data)
 
     elif request.method == 'PUT':
+        old_amount =  expense.amount
         serializer = ExpenseSerializer(expense, data=request.data)
         if serializer.is_valid():
+            new_amount = serializer.validated_data['amount']
+            funding = expense.funding_id
+            total_expense_lain = Expense.objects.filter(funding_id=funding).exclude(id=expense.id).aaggregate(total=Sum('amount'))['total'] or 0
+
+            if total_expense_lain + new_amount > funding.amount + old_amount:
+                return Response({'error': 'dana tidak mencukupi untuk update'},status=status.HTTP_400_BAD_REQUEST)
+            
+            funding.amount += old_amount
+            funding.amount -= new_amount
+            funding.save()
+
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
+        funding = expense.funding_id
+        refund_amount = expense.amount
+        funding.amount +=refund_amount
+        funding.save() 
         expense.delete()
         return Response({'message': 'Deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
 
