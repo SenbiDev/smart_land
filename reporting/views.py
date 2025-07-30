93a15fb0ee52b3115cf9187b273feeaf74cd6e99
faizulhq <faizulhaq1011@gmail.com>
2025-07-30 14:02:25 +0700

    Modified Reporting Module

--- reporting/views.py
+++ reporting/views.py
@@ -11,13 +11,16 @@ from production.models import Production
 def laporan_keuangan(request):
     total_dana = Funding.objects.aggregate(total=Sum('amount'))['total'] or 0
     total_pengeluaran = Expense.objects.aggregate(total=Sum('amount'))['total'] or 0
-
-    total_yield = Production.objects.aggregate(
-        total=Sum(F('quantity') * F('unit_price'), output_field=DecimalField())
-    )['total'] or 0
-
+    total_yield = Production.objects.aggregate(total=Sum(F('quantity') * F('unit_price'), output_field=DecimalField()))['total'] or 0
     laba_rugi = total_yield - total_pengeluaran
     sisa_dana = total_dana - total_pengeluaran
+
+    if laba_rugi > 0:
+        status_keuangan = "Laba"
+    elif laba_rugi < 0:
+        status_keuangan = "Rugi"
+    else:
+        status_keuangan = "Impas"
 
     data = {
         "ringkasan_dana": {
@@ -26,7 +29,10 @@ def laporan_keuangan(request):
             "sisa_dana": float(sisa_dana)
         },
         "total_yield": float(total_yield),
-        "laba_rugi": float(laba_rugi)
+        "laba_rugi": {
+            "Jumlah": float(laba_rugi),
+            "Status": status_keuangan
+        }
     }
 
     return Response(data, status=status.HTTP_200_OK)
