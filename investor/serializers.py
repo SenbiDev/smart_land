bbb4b000e88a0e43cc0fda384f325665f80c5b0f
faizulhq <faizulaq1011@gmail.com>
2025-10-24 00:06:00 +0700

    modified investor

--- investor/serializers.py
+++ investor/serializers.py
@@ -5,8 +5,11 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class InvestorSerializer(serializers.ModelSerializer):
-    user = serializers.PrimaryKeyRelatedField(read_only=True)
-
+    # Tampilkan info user untuk response
+    username = serializers.CharField(source='user.username', read_only=True)
+    email = serializers.CharField(source='user.email', read_only=True)
+    
     class Meta:
         model = Investor
-        fields = ['id', 'user', 'contact', 'join_date', 'total_investment']
+        fields = ['id', 'user', 'username', 'email', 'contact', 'join_date', 'total_investment']
+        read_only_fields = ['user']  # user akan di-set otomatis dari request.user
