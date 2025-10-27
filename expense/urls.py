068246808d263477ce15b40b9421fb230cae2acf
raflir13 <rafli.ramdhani1313@gmail.com>
2025-10-27 14:50:58 +0700

    fix urls expense

--- expense/urls.py
+++ expense/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('expensess/', views.expense_list_create),
-    path('expensesss/<int:pk>/', views.expense_detail_update_delete),
-    path('expenses/<int:funding_id>/expense-summary/', views.funding_expense_summary),
+    path('expenses/', views.list_expense),
+    path('expenses/<int:pk>/', views.expense_detail),
 ]
